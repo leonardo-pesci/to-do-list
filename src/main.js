bca1e0a04361a6d36d6169249b974e113b2a2090
leonardo-pesci <leonardo.pesci.2003@gmail.com>
2024-04-29 17:01:04 +0200

    titles

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-/* Elements */
+//^ Elements
 const emptyMessage = document.querySelector('#emptyMessage')
 const todoContainer = document.querySelector('#todoContainer')
 const doneContainer = document.querySelector('#doneContainer')
 const todoInput = document.querySelector('#todoInput')
 const todoAddBtn = document.querySelector('#todoAddBtn')
 
-/* Variables */
+//^ Variables
 let todoList = []
 const todoStorage = localStorage.getItem('todoList')
 if (todoStorage) todoList = JSON.parse(todoStorage)
@@ -14,7 +14,7 @@ let doneList = []
 const doneStorage = localStorage.getItem('doneList')
 if (doneStorage) doneList = JSON.parse(todoStorage)
 
-/* Functions */
+//^ Functions
 let buttonEvent = () => {
     if (todoInput.value !== '') {
         todoList.push(todoInput.value.trim())
@@ -25,29 +25,37 @@ let buttonEvent = () => {
     }
 }
 
-let createItem = (text, i) => {
+let createItem = (type, text, i) => {
     let item = document.createElement('li')
     item.id = 'item' + i
-    item.className = 'todoItem panel d-flex'
-    item.innerHTML = `<div id="check${i}" class="todoCheck c-pointer"><img src="./img/check.svg" alt=""></div><span class="todoText">${text}</span>`
+
+    if (type === 'todo') {
+        item.className = 'todoItem panel d-flex'
+        item.innerHTML = `<div id="check${i}" class="todoCheck c-pointer"><img src="./img/check.svg" alt=""></div><span class="todoText">${text}</span>`
+
+    } else if (type === 'done') {
+        item.className = 'doneItem panel d-flex'
+        item.innerHTML = `<div id="uncheck${i}" class="doneUncheck c-pointer"><img src="./img/check.svg" alt=""></div><span class="todoText">${text}</span><div id="close${i}" class="doneClose c-pointer"><img src="./img/close.png" alt=""></div>`
+    }
 
     return item
 }
 
 let renderList = () => {
     todoContainer.innerHTML = ''
+    doneContainer.innerHTML = ''
 
     if (todoList.length === 0) emptyMessage.classList.remove('visually-hidden')
     else emptyMessage.classList.add('visually-hidden')
 
     todoList.forEach( (item, i) => {
-        let element = createItem(item, i)
+        let element = createItem('todo', item, i)
 
         todoContainer.appendChild(element)
     })
 
     doneList.forEach( (item, i) => {
-        let element = createItem(item, i)
+        let element = createItem('done', item, i)
 
         doneContainer.appendChild(element)
     })
@@ -64,12 +72,22 @@ let renderList = () => {
             renderList()
         })
     })
+
+    const unchecks = document.querySelectorAll('.todoCheck') //!
+    checks.forEach( (uncheck, i) => {
+        uncheck.addEventListener('click', () => {
+            todoList.push(todoList[i])
+            doneList.splice(i, 1)
+
+            renderList()
+        })
+    })
 }
 
 renderList()
 
 
-/* Events */
+//^ Events
 todoAddBtn.addEventListener('click', buttonEvent)
 
 document.addEventListener('keydown', (e) => {
